4e412afb48ffa415bdfb7de79ff5b054ac81851c
Hongcheng Zhu <hcz@fb.com>
2017-06-09 20:36:48 -0700

    update

--- digital-recognizer/data_util.py
+++ digital-recognizer/data_util.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt, matplotlib.image as mpimg
 
 # %matplotlib inline
 
-def load_data(name, max_rows=None):
+def load_data(name, max_rows=None, normalize=False):
     img_dim = 28
     labels_and_imgs = genfromtxt(name, delimiter=',', skip_header=1, max_rows=max_rows)
     print('There are {} instances with {} pixels.'.format(len(labels_and_imgs), len(labels_and_imgs[0])))
@@ -15,10 +15,17 @@ def load_data(name, max_rows=None):
     if len(labels_and_imgs[0,:]) == img_dim * img_dim + 1:
         labels = labels_and_imgs[:,0]
         labels_and_imgs = labels_and_imgs[:,1:]
+    if normalize:
+        for img in labels_and_imgs:
+            avg = img.sum() / (img > 0).sum()
+            std = img.std()
+            thrshold = avg - std
+            img[img <= thrshold] = 0
+            img[img > thrshold] = 1
     return labels_and_imgs.reshape(len(labels_and_imgs), img_dim, img_dim), labels
 
-def show_imgs(name, max_rows=None):
-    imgs, labels = load_data(name, max_rows)
+def show_imgs(name, max_rows=100, normalize=False):
+    imgs, labels = load_data(name, max_rows, normalize)
     print('There {} images.'.format(len(imgs)))
     for i in range(len(imgs)):
         img = imgs[i]
@@ -29,4 +36,4 @@ def show_imgs(name, max_rows=None):
     plt.show()
 
 if __name__ == "__main__":
-    show_imgs(sys.argv[1], int(sys.argv[2]))
+    show_imgs(sys.argv[1], int(sys.argv[2]), len(sys.argv) > 3 and sys.argv[3] == "norm")
